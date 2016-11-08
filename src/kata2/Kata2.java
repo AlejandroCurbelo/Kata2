dbfc33dc8f5495d5d0bdddbfe0895be5c1e2cdcd
alejandro <alejandro@alejandro-K55VD>
2016-11-08 17:47:11 +0000

    Segunda versión de Kata2 para valores genéricos (prueba con String).

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,13 +6,14 @@ public class Kata2 {
     
      public static void main(String[] args) {
          
-        Double[] vector = {2.0,1.0,3.0,1.0,4.0,6.0,100.9,100.9,2.0,100.9,100.9};
+        //Double[] vector = {2.0,1.0,3.0,1.0,4.0,6.0,100.9,100.9,2.0,100.9,100.9};
+        String[] vector = {"Alejandro", "Kilian", "Alguien", "Alejandro", "Alejandro"};
         
         Histogram histo = new Histogram(vector);
         
-        HashMap<Double,Integer> histogram = histo.getHashMap();
+        HashMap<Object,Integer> histogram = histo.getHashMap();
         
-        for(Double key : histogram.keySet()){
+        for(Object key : histogram.keySet()){
             System.out.println(key + " -> " + histogram.get(key));
         }
     }
